12d814e3a8662b6e65a2c7d22c09ad971b5c1994
collenirwin <collenirwin@gmail.com>
2018-03-08 20:11:02 -0500

    update setup

--- PadSharp/MainView.xaml.cs
+++ PadSharp/MainView.xaml.cs
@@ -11,7 +11,6 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
