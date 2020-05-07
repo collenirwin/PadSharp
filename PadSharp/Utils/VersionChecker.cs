934073245985a30c6a83eab95102fe506554c3de
collenirwin <collenirwin@gmail.com>
2020-05-07 15:54:01 -0400

    update setup (2.1.0)

--- PadSharp/Utils/VersionChecker.cs
+++ PadSharp/Utils/VersionChecker.cs
@@ -12,7 +12,7 @@ namespace PadSharp.Utils
         /// <summary>
         /// Version of the currently running application
         /// </summary>
-        public const string Version = "2.0.1";
+        public const string Version = "2.1.0";
 
         /// <summary>
         /// URL to the version file on GitHub
