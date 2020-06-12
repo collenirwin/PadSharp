8918fcb7ba102db6ba5999df685253e2eafbf91d
collenirwin <collenirwin@gmail.com>
2020-06-12 09:48:16 -0400

    normalize line endings, clean up Crypto docstrings, format

--- PadSharp/Utils/Crypto.cs
+++ PadSharp/Utils/Crypto.cs
@@ -6,57 +6,55 @@ using System.Text;
 
 namespace PadSharp.Utils
 {
+    /// <summary>
+    /// Contains methods for encrypting, decrypting, and hashing strings.
+    /// </summary>
     public static class Crypto
     {
         private const int _keyByteCount = 32;
-        private const int _pbkdf2Iterations = 10000;
+        private const int _pbkdf2Iterations = 10_000;
 
         /// <summary>
-        /// Uses SHA256 to hash a string (password) with the specified salt added
+        /// Uses SHA256 to hash a string (password) with the specified salt added.
         /// </summary>
-        /// <param name="password">password to hash</param>
-        /// <param name="salt">salt string to add to the password hash</param>
-        /// <returns>the hashed password</returns>
+        /// <param name="password">Password to hash.</param>
+        /// <param name="salt">Salt string to add to the password hash.</param>
+        /// <returns>The hashed password.</returns>
         public static string Hash(string password, string salt)
         {
             var hashString = new StringBuilder();
 
-            // sha256 hash
-            using (var sha256 = new SHA256Managed())
-            {
-                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password + salt));
+            using var sha256 = new SHA256Managed();
+            byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password + salt));
 
-                foreach (byte b in bytes)
-                {
-                    // append bytes in base 16
-                    hashString.Append(b.ToString("x2"));
-                }
+            foreach (byte b in bytes)
+            {
+                // append bytes in base 16
+                hashString.Append(b.ToString("x2"));
             }
 
             return hashString.ToString();
         }
 
         /// <summary>
-        /// Checks to see if a plaintext password with salt matches a hashed password
+        /// Checks to see if a plaintext password with salt matches a hashed password.
         /// </summary>
-        /// <param name="password">plaintext password</param>
-        /// <param name="salt">salt string to add</param>
-        /// <param name="hashString">hashed password to check against</param>
-        /// <returns>true if the password matches</returns>
-        public static bool HashIsMatch(string password, string salt, string hashString)
-        {
-            return Hash(password, salt) == hashString;
-        }
+        /// <param name="password">Plaintext password/</param>
+        /// <param name="salt">Salt string to add.</param>
+        /// <param name="hashString">Hashed password to check against.</param>
+        /// <returns>true if the password matches.</returns>
+        public static bool HashIsMatch(string password, string salt, string hashString) =>
+            Hash(password, salt) == hashString;
 
         /// <summary>
-        /// Uses Rijndael to encrypt the specified text with the specified password
+        /// Uses Rijndael to encrypt the specified text with the specified password.
         /// </summary>
-        /// <param name="text">text to encrypt</param>
-        /// <param name="password">password to use</param>
-        /// <returns>the encrypted string</returns>
+        /// <param name="text">Text to encrypt.</param>
+        /// <param name="password">Password to use.</param>
+        /// <returns>The encrypted text.</returns>
         public static string Encrypt(string text, string password)
         {
-            // generate random salt and initialization vector, we will prepend to string later
+            // generate random salt and initialization vector
             var saltBytes = GenerateRandomByteArray(_keyByteCount);
             var ivBytes = GenerateRandomByteArray(_keyByteCount);
             var textBytes = Encoding.UTF8.GetBytes(text);
@@ -69,27 +67,29 @@ namespace PadSharp.Utils
             using var encryptor = aes.CreateEncryptor(keyBytes, ivBytes);
             using var memoryStream = new MemoryStream();
             using var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
+
             // encrypt all text
             cryptoStream.Write(textBytes, 0, textBytes.Length);
 
             // clear the buffer
             cryptoStream.FlushFinalBlock();
 
-            // prepend the salt and iv to the encrypted bytes
-            var finalBytes = saltBytes; // salt
-            finalBytes = finalBytes.Concat(ivBytes).ToArray(); // IV
-            finalBytes = finalBytes.Concat(memoryStream.ToArray()).ToArray(); // the rest
+            // salt + iv + encrypted bytes
+            var finalBytes = saltBytes
+                .Concat(ivBytes)
+                .Concat(memoryStream.ToArray())
+                .ToArray();
 
             // convert our byte array to string and return
             return Convert.ToBase64String(finalBytes);
         }
 
         /// <summary>
-        /// Decrypts text that was previously encrypted with Crypto.encrypt
+        /// Decrypts text that was previously encrypted with <see cref="Encrypt(string, string)"/>.
         /// </summary>
-        /// <param name="encryptedText">previously encrypted text</param>
-        /// <param name="password">password to encrypt decrypt with</param>
-        /// <returns>the decrypted string</returns>
+        /// <param name="encryptedText">Encrypted text.</param>
+        /// <param name="password">Password to use in decryption.</param>
+        /// <returns>The decrypted string.</returns>
         public static string Decrypt(string encryptedText, string password)
         {
             // convert our encrypted string back to a byte array
@@ -105,7 +105,6 @@ namespace PadSharp.Utils
             var encryptedBytes = allBytes.Skip(_keyByteCount * 2).ToArray();
 
             using var key = new Rfc2898DeriveBytes(password, saltBytes, _pbkdf2Iterations);
-
             byte[] keyBytes = key.GetBytes(_keyByteCount);
 
             using var aes = CreateAes();
@@ -129,29 +128,25 @@ namespace PadSharp.Utils
         /// sets Mode to CBC,
         /// sets Padding to PKCS7
         /// </summary>
-        private static RijndaelManaged CreateAes()
-        {
-            return new RijndaelManaged
+        private static RijndaelManaged CreateAes() =>
+            new RijndaelManaged
             {
                 BlockSize = 256,
                 Mode = CipherMode.CBC,
                 Padding = PaddingMode.PKCS7
             };
-        }
 
         /// <summary>
-        /// Generates an array of random bytes of a specified size
+        /// Generates an array of random bytes of a specified size.
         /// </summary>
-        /// <param name="numberOfBytes">size of the array in bytes</param>
-        /// <returns>an array of random bytes</returns>
+        /// <param name="numberOfBytes">Size of the array.</param>
+        /// <returns>An array of random bytes.</returns>
         public static byte[] GenerateRandomByteArray(int numberOfBytes)
         {
             var bytes = new byte[numberOfBytes];
 
-            using (var ran = new RNGCryptoServiceProvider())
-            {
-                ran.GetBytes(bytes);
-            }
+            using var random = new RNGCryptoServiceProvider();
+            random.GetBytes(bytes);
 
             return bytes;
         }
--- PadSharp/Utils/TextEditorUtils.cs
+++ PadSharp/Utils/TextEditorUtils.cs
@@ -1,6 +1,5 @@
 ﻿using ICSharpCode.AvalonEdit;
 using System;
-using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace PadSharp.Utils
