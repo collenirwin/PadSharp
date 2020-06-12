202f177db8db0889842d0058e47580dc2814ca61
collenirwin <collenirwin@gmail.com>
2020-06-12 09:24:05 -0400

    clean up StringUtils docstrings, minor formatting

--- PadSharp/Utils/StringUtils.cs
+++ PadSharp/Utils/StringUtils.cs
@@ -6,51 +6,43 @@ using System.Threading.Tasks;
 namespace PadSharp.Utils
 {
     /// <summary>
-    /// Contains extension methods for manipulating string
+    /// Contains extension methods for manipulating string.
     /// </summary>
     public static class StringUtils
     {
         /// <summary>
-        /// Splits a string into lines, regardless of line ending style
+        /// Splits a string into lines, regardless of line ending style.
         /// </summary>
-        /// <param name="lines">lines to split</param>
-        /// <returns>lines separated into an array</returns>
-        public static string[] SplitIntoLines(this string lines)
-        {
-            return lines.RegexSplit(@"\r?\n");
-        }
+        /// <param name="lines">Lines to split.</param>
+        /// <returns>Lines separated into an array.</returns>
+        public static string[] SplitIntoLines(this string lines) => lines.RegexSplit(@"\r?\n");
 
         /// <summary>
-        /// Reverses the order of the specified lines
+        /// Reverses the order of the lines in the given string.
         /// </summary>
-        /// <param name="linesToReverse">Lines to reverse</param>
-        /// <returns>linesToReverse in reverse order</returns>
-        public static string ReverseLines(this string linesToReverse)
-        {
-            return string.Join("\r\n", linesToReverse.SplitIntoLines().Reverse());
-        }
+        /// <param name="linesToReverse">String containing lines to reverse.</param>
+        /// <returns>The given string with its lines reversed.</returns>
+        public static string ReverseLines(this string linesToReverse) =>
+            string.Join("\r\n", linesToReverse.SplitIntoLines().Reverse());
 
         /// <summary>
-        /// Sorts the specified lines alphabetically
+        /// Sorts the order of the lines in the given string.
         /// </summary>
-        /// <param name="linesToSort">Lines to sort</param>
+        /// <param name="linesToSort">String containing lines to sort.</param>
         /// <param name="descending">Sort in descending order?</param>
-        /// <returns>Sorted linesToSort</returns>
+        /// <returns>The given string with its lines sorted.</returns>
         public static string SortLines(this string linesToSort, bool descending = false)
         {
-            // get a collection of the lines
             var lines = linesToSort.SplitIntoLines();
-
-            // sort 'em (ascending or descending based on passed param)
             var sortedlines = descending ? lines.OrderByDescending(x => x) : lines.OrderBy(x => x);
             return string.Join("\r\n", sortedlines);
         }
 
         /// <summary>
-        /// Converts the specified text to Titlecase
+        /// Converts the specified text to Titlecase.
         /// </summary>
-        /// <param name="textToConvert">Text you want to convert to Titlecase</param>
-        /// <returns>textToConvert in title case</returns>
+        /// <param name="textToConvert">Text to convert to Titlecase.</param>
+        /// <returns>The given text, converted to Titlecase.</returns>
         public static string ToTitleCase(this string textToConvert)
         {
             string newText = textToConvert;
@@ -58,7 +50,7 @@ namespace PadSharp.Utils
 
             foreach (Match match in matches)
             {
-                // we go through the .remove.insert ordeal here in order to preserve the whitespace between the words
+                // we go through the .Remove.Insert ordeal here in order to preserve the whitespace between the words
                 newText = newText
                     .Remove(match.Index, match.Length)
                     .Insert(match.Index, match.Value.Length == 1 // 1-length word:
@@ -70,81 +62,76 @@ namespace PadSharp.Utils
         }
 
         /// <summary>
-        /// Converts the specified text to tOGGLECASE
+        /// Converts the specified text to tOGGLECASE.
         /// </summary>
-        /// <param name="textToConvert">Text you feel needs a case toggle</param>
-        /// <returns>textToConvert in with each letter's case toggled</returns>
-        public static string ToggleCase(this string textToConvert)
-        {
-            return string.Join("", textToConvert
+        /// <param name="textToConvert">Text to convert to tOGGLECASE.</param>
+        /// <returns>The given text, converted to tOGGLECASE.</returns>
+        public static string ToggleCase(this string textToConvert) =>
+            string.Join("", textToConvert
                 .Select(c => char.IsUpper(c) ? char.ToLower(c) : char.ToUpper(c)));
-        }
 
         /// <summary>
-        /// Prepends linesToPrepend with textToPrepend
+        /// Prepends each line in linesToPrepend with textToPrepend.
         /// </summary>
-        /// <param name="linesToPrepend">Lines to prepend with textToPrepend</param>
-        /// <param name="textToPrepend">String to prepend to the passed lines</param>
-        /// <returns>linesToPrepend with textToPrepend prepended to each line</returns>
-        public static string PrependLines(this string linesToPrepend, string textToPrepend)
-        {
-            return string.Join("\r\n", linesToPrepend
+        /// <param name="linesToPrepend">Lines to prepend with textToPrepend.</param>
+        /// <param name="textToPrepend">String to prepend to the passed lines.</param>
+        /// <returns>linesToPrepend with textToPrepend prepended to each line.</returns>
+        public static string PrependLines(this string linesToPrepend, string textToPrepend) =>
+            string.Join("\r\n", linesToPrepend
                 .SplitIntoLines()
-                .Select(x => textToPrepend + x));
-        }
+                .Select(line => textToPrepend + line));
 
         /// <summary>
         /// Toggles the passed start of the lines.
         /// If the start exists, remove it. If not, add it.
         /// </summary>
-        /// <param name="lines">Lines to toggle</param>
-        /// <param name="lineStart">Start of the lines to toggle</param>
-        /// <returns></returns>
-        public static string ToggleLineStart(this string lines, string lineStart)
-        {
-            return string.Join("\r\n", lines
+        /// <param name="lines">Lines to toggle the start of.</param>
+        /// <param name="lineStart">Start of the lines to toggle.</param>
+        /// <returns>lines with lineStart toggled on each line.</returns>
+        public static string ToggleLineStart(this string lines, string lineStart) =>
+            string.Join("\r\n", lines
                 .SplitIntoLines()
-                .Select(x => x.StartsWith(lineStart) ? x.Substring(lineStart.Length) : lineStart + x));
-        }
+                .Select(line => line.StartsWith(lineStart) ? line.Substring(lineStart.Length) : lineStart + line));
 
         /// <summary>
         /// Toggles the passed start and end of the string.
         /// If the start and end exist, remove them. If not, add them.
         /// </summary>
-        /// <param name="text">Text to toggle</param>
-        /// <param name="lineStart">Start of the string to toggle</param>
-        /// <param name="lineEnd">End of the string to toggle</param>
-        /// <returns>text with start and end added or removed</returns>
-        public static string ToggleStartAndEnd(this string text, string lineStart, string lineEnd)
+        /// <param name="text">Text to toggle the start and end of.</param>
+        /// <param name="start">Start of the string to toggle.</param>
+        /// <param name="end">End of the string to toggle.</param>
+        /// <returns>text with start and end added or removed.</returns>
+        public static string ToggleStartAndEnd(this string text, string start, string end)
         {
-            bool haveStart = text.StartsWith(lineStart);
-            bool haveEnd = text.EndsWith(lineEnd);
+            bool haveStart = text.StartsWith(start);
+            bool haveEnd = text.EndsWith(end);
 
             // start and end already there
             if (haveStart && haveEnd)
             {
                 // return the string without the start and end
-                return text.Substring(lineStart.Length, text.Length - lineEnd.Length - lineStart.Length);
+                return text.Substring(start.Length, text.Length - end.Length - start.Length);
             }
             else if (haveStart)
             {
-                return text + lineEnd;
+                return text + end;
             }
             else if (haveEnd)
             {
-                return lineStart + text;
+                return start + text;
             }
 
-            return lineStart + text + lineEnd;
+            return start + text + end;
         }
 
         /// <summary>
-        /// Attempts to count the number of matches of the specified Regular Expression within the string asyncronously.
+        /// Attempts to count the number of matches of the specified
+        /// Regular Expression within the string asyncronously.
         /// </summary>
-        /// <param name="text">Text to search in</param>
-        /// <param name="regex">Regular Expression to apply to text</param>
+        /// <param name="text">Text to search within.</param>
+        /// <param name="regex">Regular Expression to apply to text.</param>
         /// <param name="matchCase">Use IgnoreCase flag?</param>
-        /// <returns>The number of matches, or null if an Exception was thrown</returns>
+        /// <returns>The number of matches, or null if an Exception was thrown.</returns>
         public static async Task<int?> TryCountMatchesAsync(this string text, string regex, bool matchCase)
         {
             try
